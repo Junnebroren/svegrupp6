9632dd9dda6247e1fc4e4f9d8989959556935f51
malmP <pallemalm@gmail.com>
2019-12-13 13:10:21 +0100

    ändrat i booking

--- src/Main/Booking.java
+++ src/Main/Booking.java
@@ -4,6 +4,55 @@ public class Booking {
     private String roomNr;
     private String customerSsn;
 
+    public Booking(String roomNr, String customerSsn, int bookingId, date checkInDate, date checkOutDate, double totalPrice) {
+        this.roomNr = roomNr;
+        this.customerSsn = customerSsn;
+        this.bookingId = bookingId;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.totalPrice = totalPrice;
+    }
+
+    private int bookingId;
+    private date checkInDate;
+    private date checkOutDate;
+    private double totalPrice;
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    private double totalPrice;
+
+
     public Booking(String roomNr, String customerSsn) {
         this.roomNr = roomNr;
         this.customerSsn = customerSsn;
